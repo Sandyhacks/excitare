36174ede770fb81d9de765ee19a55c9e5da9ec5e
jaytchheda@gmail.com <jaytarunchheda@gmail.com>
2015-10-24 10:05:29 -0400

    trying init commit

--- app/src/main/java/com/example/iyers_000/excitarehackumass/MainActivity.java
+++ app/src/main/java/com/example/iyers_000/excitarehackumass/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 //lets see if this works
+// let me try too :)
 public class MainActivity extends AppCompatActivity {
 
     @Override
